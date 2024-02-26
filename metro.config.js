627dbf84e3cb6c740f88cb9980fcfa9ab161bc70
umutbas <umutbasinfo@gmal.com>
2024-02-26 14:37:21 +0300

    Twitter UI Clone

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,27 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const {
+    resolver: {sourceExts, assetExts},
+  } = defaultConfig;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return mergeConfig(defaultConfig, {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  });
+};
+
+module.exports = config;
