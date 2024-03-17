f2cc7ce9946f030525024bdc031b639542765ab2
umutbas <umutbasinfo@gmal.com>
2024-03-17 16:55:45 +0300

    Tweets and replices page

--- src/pages/Profile/TweetsAndReplies/TweetsAndReplies.js
+++ src/pages/Profile/TweetsAndReplies/TweetsAndReplies.js
@@ -1,12 +1,116 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ScrollView} from 'react-native';
+import React from 'react';
+
+import styles from './TweetsAndReplies.style';
+
+import ActionButtons from '../../../assets/svgs/repliceActionButtons.svg';
+import Image1 from '../../../assets/svgs/ımageProfile2.svg';
+import Image2 from '../../../assets/svgs/ımageProfile3.svg';
+import User from '../../../assets/svgs/user.svg';
+import DownIcon from '../../../assets/svgs/downIcon.svg';
+import KomolAvatar from '../../../assets/svgs/komolAvatar.svg';
+import Polygon from '../../../assets/svgs/polygonImage.svg';
 
 const TweetsAndReplies = () => {
   return (
-    <View>
-      <Text>TweetsAndReplies</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      {/* pixels tweet */}
+      <View style={styles.twittContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.twittIcon}>
+            <User width="50" height="50" />
+          </View>
+          <View style={{margin: 5}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.userName}>Pixsellz</Text>
+              <Text style={styles.userTag}>@pixsellz</Text>
+              <Text
+                style={{
+                  ...styles.userTag,
+                  marginRight: 80,
+                }}>
+                · 2/14/20
+              </Text>
+              <DownIcon />
+            </View>
 
-export default TweetsAndReplies
+            <View style={styles.twittTextContainer}>
+              <Text style={styles.twittText}>Must have icon collections</Text>
+            </View>
+
+            <View style={{flexDirection: 'row'}}>
+              <View style={styles.imageBorderContainer}>
+                <Image1 width="80" height="80" resizeMode="contain" />
+              </View>
+              <View
+                style={{
+                  ...styles.imageBorderContainer,
+                  width: 200,
+                  borderTopRightRadius: 5,
+                  borderBottomRightRadius: 5,
+                }}>
+                <Text style={{marginHorizontal: 5}}>
+                  The Best Free Icon Packs Everyone Must Download i...{'\n\n'}
+                  <Text style={styles.twittText}>graphicmama.com</Text>
+                </Text>
+              </View>
+            </View>
+
+            <ActionButtons />
+          </View>
+        </View>
+      </View>
+
+      {/* komol tweet */}
+
+      <View style={styles.twittContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.twittIcon}>
+            <KomolAvatar width="50" height="50" />
+          </View>
+          <View style={{margin: 5}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text style={styles.userName}>Komol Kuchkarov</Text>
+              <Text style={styles.userTag}>@kkuchka... </Text>
+              <Text
+                style={{
+                  ...styles.userTag,
+                  marginRight: 5,
+                }}>
+                ·2/13/20
+              </Text>
+              <DownIcon />
+            </View>
+
+            <View style={styles.twittTextContainer}>
+              <Text style={styles.twittText}>
+                Quickly create a low-fi wireframe version of your web projects
+                with ready-to-use layouts of Scheme Constructor.{'\n\n'}Get it
+                now on 👉{' '}
+                <Text style={{color: 'rgb(97, 156, 229)', marginBottom: 10}}>
+                  constructor.pixsellz.io
+                </Text>
+              </Text>
+            </View>
+
+            <View style={styles.imageContainer}>
+              <Image2 width="300" height="100" />
+              <View style={styles.polygon}>
+                <View style={styles.polygonContainer}>
+                  <Polygon width="17" height="17" />
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default TweetsAndReplies;
---
+++ src/pages/Profile/TweetsAndReplies/TweetsAndReplies.style.js
@@ -0,0 +1,81 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.2,
+    borderColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+  twittContainer: {
+    marginTop: 10,
+    marginBottom: 3,
+    paddingRight: 5,
+    paddingLeft: 8,
+    borderBottomWidth: 0.2,
+    borderColor: 'grey',
+  },
+
+  twittIcon: {
+    alignItems: 'center',
+    marginLeft: 8,
+  },
+  userName: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginLeft: 4,
+  },
+  userTag: {
+    fontSize: 15,
+    marginLeft: 5,
+    color: 'grey',
+  },
+  twittTextContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+    width: 300,
+    marginBottom: 5,
+  },
+  twittText: {
+    color: 'grey',
+    marginRight: 10,
+    marginLeft: 5,
+  },
+  thread: {
+    color: 'rgb(97, 156, 229)',
+    marginBottom: 10,
+  },
+  polygonContainer: {
+    backgroundColor: '#4C9EEB',
+    borderRadius: 50,
+    width: 47,
+    height: 47,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 3,
+    borderColor: 'white',
+  },
+  imageBorderContainer: {
+    position: 'relative',
+    marginBottom: 30,
+    borderWidth:0.2,
+    borderColor:'grey',
+    width:81,
+    height:80,
+  },
+
+  polygon: {
+    marginTop: -30,
+    marginHorizontal: 120,
+  },
+  imageContainer: {
+    position: 'relative',
+    marginBottom: 30,
+  },
+});
