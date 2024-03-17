ee3c3e9b0d53f88d7da29439220fdd39c52aace4
umutbas <umutbasinfo@gmal.com>
2024-03-17 16:55:32 +0300

    Tweets page

--- src/pages/Profile/Tweets/Tweets.js
+++ src/pages/Profile/Tweets/Tweets.js
@@ -1,12 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ScrollView} from 'react-native';
+import React from 'react';
+
+import styles from './Tweets.styles';
+
+import Pin from '../../../assets/svgs/pinIcon.svg';
+import ActionButtons from '../../../assets/svgs/profileActionButtons.svg';
+import Image from '../../../assets/svgs/ımageProfile.svg';
+import Polygon from '../../../assets/svgs/polygonImage.svg';
+import User from '../../../assets/svgs/user.svg';
+import DownIcon from '../../../assets/svgs/downIcon.svg';
 
 const Tweets = () => {
   return (
-    <View>
-      <Text>Tweets</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      <View style={styles.twittContainer}>
+        <View style={styles.likeContainer}>
+          <Pin />
+          <Text style={styles.likeText}>Pinned Tweet</Text>
+        </View>
 
-export default Tweets
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.twittIcon}>
+            <User width="50" height="50" />
+          </View>
+          <View style={{margin: 5}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.userName}>Pixsellz</Text>
+              <Text style={styles.userTag}>@pixsellz</Text>
+              <Text
+                style={{
+                  ...styles.userTag,
+                  marginRight: 80,
+                }}>
+                · 7/31/19
+              </Text>
+              <DownIcon />
+            </View>
+
+            <View style={styles.twittTextContainer}>
+              <Text style={styles.twittText}>
+                Scheme Constructor - your ultimate prototyping kit for all UX
+                web and mobileepp design!{'\n'}
+                <Text style={{color: 'rgb(97, 156, 229)', marginBottom: 10}}>
+                  constructor.pixsellz.io{'\n\n'}#prototyping #wireframe #uiux
+                  #ux
+                </Text>
+              </Text>
+            </View>
+
+            <View style={styles.imageContainer}>
+              <Image width="310" height="200" />
+              <View style={styles.polygon}>
+                <View style={styles.polygonContainer}>
+                  <Polygon width="17" height="17" />
+                </View>
+              </View>
+            </View>
+
+            <Text style={styles.imageText}>109 views</Text>
+            <ActionButtons />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Tweets;
--- src/pages/Profile/Tweets/Tweets.styles.js
+++ src/pages/Profile/Tweets/Tweets.styles.js
@@ -1,3 +1,84 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({})
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.2,
+    borderColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+  twittContainer: {
+    marginTop: 10,
+    marginBottom: 3,
+    paddingRight: 5,
+    paddingLeft: 8,
+  },
+  likeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 47,
+  },
+  likeText: {
+    marginLeft: 10,
+    color: 'grey',
+  },
+  twittIcon: {
+    alignItems: 'center',
+    marginLeft: 8,
+  },
+  userName: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginLeft: 4,
+  },
+  userTag: {
+    fontSize: 17,
+    marginLeft: 5,
+    color: 'grey',
+  },
+  twittTextContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+    width: 300,
+    marginBottom: 5,
+  },
+  twittText: {
+    color: 'grey',
+    marginRight: 10,
+    marginLeft: 5,
+  },
+  thread: {
+    color: 'rgb(97, 156, 229)',
+    marginBottom: 10,
+  },
+  polygonContainer: {
+    backgroundColor: '#4C9EEB',
+    borderRadius: 50,
+    width: 47,
+    height: 47,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 3,
+    borderColor: 'white',
+  },
+  imageContainer: {
+    position: 'relative',
+    marginBottom: 30,
+  },
+  imageText: {
+    color: 'grey',
+    marginVertical: 8,
+    marginTop: 40,
+    marginBottom: 8,
+  },
+  polygon: {
+    marginTop: -120,
+    marginHorizontal: 120,
+  },
+});
