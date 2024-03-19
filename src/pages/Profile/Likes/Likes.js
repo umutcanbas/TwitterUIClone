e7884974abd37655c2abd0e5c8317c290e52ac1c
umutbas <umutbasinfo@gmal.com>
2024-03-19 16:09:57 +0300

    Like page

--- src/pages/Profile/Likes/Likes.js
+++ src/pages/Profile/Likes/Likes.js
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ScrollView} from 'react-native';
+import React from 'react';
 
-const Likes = () => {
+import styles from './Likes.style';
+
+import KomolAvatar from '../../../assets/svgs/komolAvatar.svg';
+import ActionButton from '../../../assets/svgs/likesActionButtons.svg';
+import Image from '../../../assets/svgs/likesImage.svg';
+import DownIcon from '../../../assets/svgs/downIcon.svg';
+
+const Media = () => {
   return (
-    <View>
-      <Text>Likes</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      <View style={styles.twittContainer}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.twittIcon}>
+            <KomolAvatar width="50" height="50" />
+          </View>
+          <View style={{margin: 5}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.userName}>Komol Kuchkarov</Text>
+              <Text style={styles.userTag}>@kkuchkarov</Text>
+              <Text
+                style={{
+                  ...styles.userTag,
+                  marginRight: 20,
+                }}>
+                · 6d
+              </Text>
+              <DownIcon />
+            </View>
 
-export default Likes
+            <View style={styles.twittTextContainer}>
+              <Text style={styles.twittText}>
+                When we first launched Vector Mockups, I had wrote that in 2018
+                our free product downloads was 28K+ and we had set a goal to
+                double this figure by the end of 2019. Today our team and I are
+                glad to announce tgat we aave easily hit our goals with 47k+
+                downloads in 2019.
+               
+              </Text>
+            </View>
+
+            <View style={styles.imageContainer}>
+              <Image width="310" height="200" />
+              <ActionButton />
+            </View>
+            <Text style={{color: 'rgb(97, 156, 229)', marginBottom: 10}}>
+                Show this thread
+                </Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Media;
