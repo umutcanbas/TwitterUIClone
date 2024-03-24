40f5dfc5499c92d62e2932a2a35320641c974964
umutbas <umutbasinfo@gmal.com>
2024-03-24 17:06:20 +0300

    Krenne tweet page

---
+++ src/components/TweetCards/KarenneTweet/KrenneTweet.js
@@ -0,0 +1,206 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import React from 'react';
+
+import styles from './KrenneTweet.style';
+import User from '../../../assets/svgs/user.svg';
+import KranneAvatar from '../../../assets/svgs/kranneAvatar.svg';
+import KeironAvatar from '../../../assets/svgs/keironAvatar.svg';
+import DownIcon from '../../../assets/svgs/downIcon.svg';
+import BackArrow from '../../../assets/svgs/leftArrowIcon.svg';
+import CommentIcon from '../../../assets/svgs/commentStrokeIcon.svg';
+import ShareIcon from '../../../assets/svgs/shareStrokeIcon.svg';
+import HeartIcon from '../../../assets/svgs/heartStrokeIcon.svg';
+import RetweetIcon from '../../../assets/svgs/retweetStrokeIcon.svg';
+
+import Home from '../../../assets/svgs/homePageIconActive.svg';
+import Search from '../../../assets/svgs/searchPageIcon.svg';
+import Notification from '../../../assets/svgs/notificationPageIcon.svg';
+import Message from '../../../assets/svgs/messagePageIcon.svg';
+
+import Modal from '../../../components/Modal/Modal';
+
+const KrenneTweet = ({navigation}) => {
+  const [inputModalVisible, setInputModalVisible] = React.useState(false);
+
+  const handleModalClose = () => {
+    setInputModalVisible(!inputModalVisible);
+  };
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={goBack}>
+            <BackArrow />
+          </TouchableOpacity>
+          <Text style={styles.headerContainerText}>Tweet</Text>
+        </View>
+        <View style={{borderColor: 'grey', borderTopWidth: 0.2}}>
+          <View style={styles.likeContainer}>
+            <RetweetIcon width="15" height="15" />
+            <Text style={styles.likeText}>The UX Person Retweeted</Text>
+          </View>
+        </View>
+
+        <View style={styles.twittContainer}>
+          <View style={{flexDirection: 'row'}}>
+            <View style={styles.twittIcon}>
+              <KranneAvatar width="50" height="50" />
+            </View>
+            <View style={{margin: 5}}>
+              <View style={{flexDirection: 'row'}}>
+                <View>
+                  <Text style={styles.userName}>karennne</Text>
+                  <Text style={styles.userTag}>@karennne</Text>
+                </View>
+                <View style={{marginLeft: 200}}>
+                  <DownIcon />
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.twittTextContainer}>
+            <Text style={styles.twittText}>
+              ~~ hiring for a UX Lead in Sydney - who should I talk to?
+            </Text>
+          </View>
+
+          <View style={styles.component}>
+            <Text style={styles.userTag}>
+              09:28 · 2/21/20 · <Text style={styles.link}>Twitter Web App</Text>
+            </Text>
+          </View>
+
+          <View style={styles.component}>
+            <Text style={styles.userTag}>
+              <Text style={styles.userName}>6</Text> Retweets
+            </Text>
+            <Text style={styles.userTag}>
+              <Text style={styles.userName}>15</Text> Likes
+            </Text>
+          </View>
+          <View style={styles.actionButtons}>
+            <CommentIcon />
+            <TouchableOpacity onPress={handleModalClose}>
+              <RetweetIcon />
+            </TouchableOpacity>
+            <HeartIcon />
+            <ShareIcon />
+          </View>
+        </View>
+
+        {/* Reply1 */}
+
+        <View style={{borderColor: 'grey', borderTopWidth: 0.2}}>
+          <View style={styles.replyTwittContainer}>
+            <View style={{flexDirection: 'row'}}>
+              <View style={styles.twittIcon}>
+                <KeironAvatar width="50" height="50" />
+              </View>
+              <View style={{margin: 5}}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.userName}>kiero_d</Text>
+                  <Text style={styles.userTag}>@kiero_d</Text>
+                  <Text style={styles.userTag}>·2d</Text>
+                  <View style={{marginLeft: 125}}></View>
+                  <DownIcon />
+                </View>
+                <Text style={styles.replyInnerText}>
+                  Replying to <Text style={styles.link}>@karennne</Text>
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.replyTwittTextContainer}>
+            <Text style={styles.replyTwittText}>
+              Interesting Nicola that not one reply or tag on this{' '}
+              <Text style={styles.link}>#UX</Text> talent shout out in the 24hrs
+              since your tweet here......🤔
+            </Text>
+          </View>
+        </View>
+        <View style={styles.replyActionButtons}>
+          <View style={{flexDirection: 'row'}}>
+            <CommentIcon width="15" height="15" />
+            <Text> 1</Text>
+          </View>
+          <TouchableOpacity onPress={handleModalClose}>
+            <RetweetIcon width="19" height="19" />
+          </TouchableOpacity>
+          <View style={{flexDirection: 'row'}}>
+            <HeartIcon width="15" height="15" />
+            <Text> 1</Text>
+          </View>
+          <ShareIcon width="15" height="15" />
+        </View>
+
+        {/* Reply 2 */}
+          <View style={styles.replyTwittContainer}>
+            <View style={{flexDirection: 'row'}}>
+              <View style={styles.twittIcon}>
+                <KranneAvatar width="50" height="50" />
+              </View>
+              <View style={{margin: 5}}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.userName}>karennne </Text>
+                  <Text style={styles.userTag}>@karennne</Text>
+                  <Text style={styles.userTag}>·2d</Text>
+                  <View style={{marginLeft: 90}}></View>
+                  <DownIcon />
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.replyTwittTextContainer}>
+            <Text style={styles.replyTwittText}>
+              Maybe I forgot the hashtags.
+              <Text style={styles.link}>
+                #hiringux #designjobs #sydneyux #sydneydesigners #uxjobs
+              </Text>
+            </Text>
+          </View>
+        <View style={styles.replyActionButtons}>
+          <View style={{flexDirection: 'row'}}>
+            <CommentIcon width="15" height="15" />
+            <Text> 1</Text>
+          </View>
+          <TouchableOpacity onPress={handleModalClose}>
+            <RetweetIcon width="19" height="19" />
+          </TouchableOpacity>
+          <HeartIcon width="15" height="15" />
+          <ShareIcon width="15" height="15" />
+        </View>
+        <Text style={{...styles.link, marginHorizontal:70, fontSize:17}}>1 more reply</Text>
+        {/* Footer */}
+
+        <View style={{flex: 1, backgroundColor: 'rgb(232, 236, 240)'}}></View>
+      <View style={styles.searchContainer}>
+        <User />
+        <TextInput style={styles.searchBar} placeholder="Add another Tweet" />
+      </View>
+
+      <View style={styles.footer}>
+        <Home />
+        <Search />
+        <Notification />
+        <Message />
+      </View>
+
+      <Modal visible={inputModalVisible} onClose={handleModalClose} />
+    </SafeAreaView>
+  );
+};
+
+export default KrenneTweet;
---
+++ src/components/TweetCards/KarenneTweet/KrenneTweet.style.js
@@ -0,0 +1,149 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+  },
+  headerContainerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 150,
+  },
+  twittContainer: {
+    marginTop: 10,
+    marginBottom: 3,
+    paddingRight: 8,
+    paddingLeft: 8,
+  },
+
+  twittIcon: {
+    alignItems: 'center',
+    marginLeft: 8,
+  },
+  userName: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginLeft: 4,
+    color: 'black',
+  },
+  userTag: {
+    fontSize: 17,
+    marginLeft: 5,
+    color: 'grey',
+  },
+  twittTextContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+    width: 300,
+    marginBottom: 15,
+    marginLeft: 10,
+  },
+  twittText: {
+    color: 'black',
+    marginRight: 10,
+    marginLeft: 5,
+    fontSize: 20,
+  },
+  link: {
+    color: 'rgb(97, 156, 229)',
+  },
+
+  component: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    borderBottomWidth: 0.2,
+    borderColor: 'grey',
+    height: 30,
+    width: 'auto',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  actionButtons: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginHorizontal: 40,
+    height: 20,
+    marginBottom: 10,
+    marginTop: 4,
+  },
+  footer: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    margin: 10,
+    width: 'auto',
+    height: 20,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+    height: 50,
+  },
+  searchBar: {
+    backgroundColor: 'rgb(232, 236, 240)',
+    borderRadius: 15,
+    width: 310,
+    height: 30,
+    marginLeft: 10,
+    fontSize: 17,
+    padding: 5,
+  },
+  ımageContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  likeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 41,
+    marginTop: 10,
+  },
+  likeText: {
+    marginLeft: 10,
+    color: 'grey',
+  },
+  replyTwittContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+    width: 'auto',
+    marginBottom: 3,
+    marginLeft: 10,
+  },
+  replyTwittTextContainer: {
+    width: 300,
+    marginBottom: 15,
+    marginLeft: 72,
+  },
+  replyTwittText: {
+    color: 'black',
+    marginRight: 10,
+    marginLeft: 5,
+    fontSize: 16,
+  },
+  replyActionButtons: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginHorizontal: 70,
+    height: 20,
+    marginBottom: 10,
+    marginTop: 4,
+    width: 300,
+  },
+  replyInnerText: {
+    color: 'grey',
+    fontSize: 14,
+    marginLeft: 3,
+    marginTop: 3,
+  },
+});
